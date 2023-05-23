0049a4b93a6c90294f30186da136a49bed0d24cb
kynk1 <metal.bird.13@gmail.com>
2023-05-23 13:31:30 +0700

    update circle CI

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=kynkyn/flask_app_img:v1
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+docker tag flask_app_image:v2 $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
